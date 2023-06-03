4aafe466490a1168b4ae0f18bb955b866bf273d4
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-06-03 09:23:53 +0900

    prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+}
