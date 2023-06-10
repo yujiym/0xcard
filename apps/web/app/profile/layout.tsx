26f1526c3c8b0660c5d2f71c576469ad28f0c447
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-06-10 11:03:41 +0900

    WIP UI

--- apps/web/app/profile/layout.tsx
+++ apps/web/app/profile/layout.tsx
@@ -1,9 +1,17 @@
 import PolybaseWrapper from '@/components/PolybaseWrapper'
+import { Toaster } from '@/components/ui/Toaster'
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <PolybaseWrapper>{children}</PolybaseWrapper>
+  return (
+    <PolybaseWrapper>
+      <>
+        {children}
+        <Toaster />
+      </>
+    </PolybaseWrapper>
+  )
 }
