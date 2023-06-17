a66eeb82f6948ff791c55c2589acc68570c214c9
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-06-18 07:00:53 +0900

    fix

--- apps/web/app/[cid]/page.tsx
+++ apps/web/app/[cid]/page.tsx
@@ -2,22 +2,30 @@ import { Metadata } from 'next'
 import UserPage from '@/components/pages/User'
 import { readData } from '@/lib/web3Storage'
 import { parseVcard } from '@/lib/vcard'
-import { siteTitle } from '@0xcard/lib/const'
+import { siteTitle, siteDescription } from '@0xcard/lib/const'
 
 type Props = {
   params: { cid: string }
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const cid = params.cid
-  // const res = await readData(cid)
-  // @ts-ignore
-  // const data: any[] = parseVcard(res)?.parsedVcard
-  // const name = data.find(el => el.property === 'FN')?.value ?? ''
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   const cid = params.cid
+//   const res = await readData(cid)
+//   // @ts-ignore
+//   const data: any[] = parseVcard(res)?.parsedVcard
+//   const name = data.find(el => el.property === 'FN')?.value ?? ''
 
-  // return {
-  //   title: name ? `${name} | ${siteTitle}` : siteTitle,
-  // }
+//   return {
+//     title: name ? `${name} | ${siteTitle}` : siteTitle,
+//   }
+// }
+
+//   return {
+//     title: name ? `${name} | ${siteTitle}` : siteTitle,
+//   }
+
+export const metadata: Metadata = {
+  title: siteDescription,
 }
 
 export default async function Page() {
