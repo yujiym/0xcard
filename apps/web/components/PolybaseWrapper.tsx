1546823175c8cbe6632a22709b62ce8a507cfe91
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-06-19 00:27:07 +0900

    add env

--- apps/web/components/PolybaseWrapper.tsx
+++ apps/web/components/PolybaseWrapper.tsx
@@ -4,8 +4,7 @@ import { Polybase } from '@polybase/client'
 import { Auth } from '@polybase/auth'
 
 export const db = new Polybase({
-  defaultNamespace:
-    'pk/0x78270b82619ae45a456613d40b07d297bb50b15fa7bad88a2eab0335fa98a82fa7f206c3c6f0ebfc5a250b2d9803935dec4eebf69eedfb1cbd28bfe7bd006711/0xcard',
+  defaultNamespace: process.env.NEXT_PUBLIC_POLYBASE_NAMESPACE || '',
 })
 
 export const auth = typeof window !== 'undefined' ? new Auth() : null
